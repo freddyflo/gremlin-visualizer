d50456393d54c2bdcf743dcbdcb8f12ebd5325cc
Fred AKLAMANU <fred.aklamanu@ibm.com>
2023-05-19 16:38:10 +0200

    update env

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://0.0.0.0:3001';
+const SERVER_URL = process.env.SERVER_URL
 export const QUERY_ENDPOINT = `${SERVER_URL}/query`;
 export const COMMON_GREMLIN_ERROR = 'Invalid query. Please execute a query to get a set of vertices';
 export const ACTIONS = {
